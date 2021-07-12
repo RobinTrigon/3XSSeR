9b977b177b150ef8c5f9b05055888693b878d51f
3rr0r-4O4 <86462148+3rr0r-4O4@users.noreply.github.com>
2021-07-13 01:57:47 +0600

    Update xss.sh

--- xss.sh
+++ xss.sh
@@ -4,3 +4,4 @@ do
 echo "$domain" | qsreplace "$2" | xargs -P30 -n1 curl -s -k -L | grep "uffufuafuafua<>/" &>/dev/null && echo -e " \033[0;31mVULNERABLE \033[0;96mURL \033[0;35m$domain" || echo -e "\033[0;98mNOT VLUN \033[0;93mURL \033[0;97m$domain"
 
 done 
+#3rr0r-404
