1c6ae8ace59fcbabb2ea41baa14b2bbe55afda40
3rr0r-4O4 <86462148+3rr0r-4O4@users.noreply.github.com>
2021-07-13 01:53:25 +0600

    Update xss.sh

--- xss.sh
+++ xss.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for domain in $(cat $1)
 do
-echo "$domain" | qsreplace "$2" | xargs -P30 -n1 curl -s -k -L | grep "uffufuafuafua<>/" &>/dev/null && echo -e " \033[0;31mVULNERABLE \033[0;96mURL \033[0;35m$domain \033[0;93mPayload---> \033[0;96m[$2]" || echo -e "\033[0;98msafe \033[0;93mURL \033[0;97m$domain"
+echo "$domain" | qsreplace "$2" | xargs -P30 -n1 curl -s -k -L | grep "uffufuafuafua<>/" &>/dev/null && echo -e " \033[0;31mVULNERABLE \033[0;96mURL \033[0;35m$domain" || echo -e "\033[0;98mNOT VLUN \033[0;93mURL \033[0;97m$domain"
 
 done 
